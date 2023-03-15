bb6d3714c69004ee13385917904b2ccffd3d2747
Jorge Sisco <jorgesisco17@gmail.com>
2023-03-15 22:26:59 +0100

    Add Javadoc comments to ExpenseTracker.java

--- src/ExpenseTracker.java
+++ src/ExpenseTracker.java
@@ -1,16 +1,37 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
+/**
+ * A simple expense tracker that allows users to add, list, and view the total of their expenses.
+ */
 public class ExpenseTracker {
+
+    /**
+     * An ArrayList to store the Expense objects.
+     */
     private final ArrayList<Expense> expenses;
 
+    /**
+     * Constructs an ExpenseTracker object with an empty list of expenses.
+     */
     public ExpenseTracker() {
         expenses = new ArrayList<>();
     }
 
+    /**
+     * Adds an expense to the list of expenses.
+     *
+     * @param expense the Expense object to add
+     */
     public void addExpense(Expense expense) {
         expenses.add(expense);
     }
 
+    /**
+     * Calculates the total amount of all expenses in the list.
+     *
+     * @return the total amount of all expenses
+     */
     public double getTotalExpenses() {
         double total = 0;
         for (Expense expense : expenses) {
@@ -19,12 +40,20 @@ public class ExpenseTracker {
         return total;
     }
 
+    /**
+     * Prints the list of expenses to the console.
+     */
     public void listExpenses() {
         for (Expense expense : expenses) {
             System.out.println(expense);
         }
     }
 
+    /**
+     * The main method that runs the expense tracker application.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         ExpenseTracker tracker = new ExpenseTracker();
         Scanner scanner = new Scanner(System.in);
