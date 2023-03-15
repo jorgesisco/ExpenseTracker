eef9c27feed2459935b324a42915a095a3c99098
Jorge Sisco <jorgesisco17@gmail.com>
2023-03-15 22:24:20 +0100

    The main method is the entry point of the application
    - The user is prompted to enter a command (add, list, total, or exit).
    - The command is read and processed using a series of conditional statements (if and else if).
    - If the command is "add", the user is prompted to enter the expense description, amount, and date. A new Expense object is created with the provided information, and it is added to the ExpenseTracker.
    - If the command is "list", the list of expenses is printed to the console.
    - If the command is "total", the total amount of all expenses is calculated and displayed.
    - If the command is "exit", the loop is terminated, and the application ends.
    - If an unknown command is entered, the user is informed and prompted to try again.

--- src/ExpenseTracker.java
+++ src/ExpenseTracker.java
@@ -24,4 +24,37 @@ public class ExpenseTracker {
             System.out.println(expense);
         }
     }
+
+    public static void main(String[] args) {
+        ExpenseTracker tracker = new ExpenseTracker();
+        Scanner scanner = new Scanner(System.in);
+        String command;
+
+        while (true) {
+            System.out.println("Enter a command (add/list/total/exit): ");
+            command = scanner.nextLine();
+
+            if (command.equalsIgnoreCase("add")) {
+                System.out.println("Enter expense description: ");
+                String description = scanner.nextLine();
+                System.out.println("Enter expense amount: ");
+                double amount = scanner.nextDouble();
+                scanner.nextLine(); // Consume newline
+                System.out.println("Enter expense date (yyyy-mm-dd): ");
+                String date = scanner.nextLine();
+
+                tracker.addExpense(new Expense(description, amount, date));
+            } else if (command.equalsIgnoreCase("list")) {
+                tracker.listExpenses();
+            } else if (command.equalsIgnoreCase("total")) {
+                System.out.printf("Total expenses: $%.2f%n", tracker.getTotalExpenses());
+            } else if (command.equalsIgnoreCase("exit")) {
+                break;
+            } else {
+                System.out.println("Unknown command. Please try again.");
+            }
+        }
+
+        scanner.close();
+    }
 }
