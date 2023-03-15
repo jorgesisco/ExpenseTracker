981810898e90f10f2f173579a76757a64881047e
Jorge Sisco <jorgesisco17@gmail.com>
2023-03-15 22:19:20 +0100

    - Added array list to store expenses - Added Constructs to ExpenseTracker object - Add expense method

--- src/ExpenseTracker.java
+++ src/ExpenseTracker.java
@@ -1,6 +1,13 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class ExpenseTracker {
+    private final ArrayList<Expense> expenses;
 
+    public ExpenseTracker() {
+        expenses = new ArrayList<>();
+    }
 
+    public void addExpense(Expense expense) {
+        expenses.add(expense);
+    }
 }
